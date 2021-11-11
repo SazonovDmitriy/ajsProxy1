7f494bed0986d3c7c0ad3da0ed334099ee61ca3d
SazonovDmitriy <sazonovd21@mail.ru>
2021-11-11 23:39:50 +0300

    1

---
+++ src/js/proxy.js
@@ -0,0 +1,14 @@
+function orderByProps () {
+
+const obj = {
+    name: 'мечник',
+    health: 10,
+    level: 2,
+    attack: 80,
+    defence: 40
+};
+
+const {name, level, attack, defence, health} = user;
+}
+
+orderByProps(obj, ["name", "level"]);
