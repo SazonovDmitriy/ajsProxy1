90b9cf8407237008b6e22d3b104f39ca96938860
SazonovDmitriy <sazonovd21@mail.ru>
2021-11-23 22:45:42 +0300

    1

--- src/js/__tests__/proxyOne.test.js
+++ src/js/__tests__/proxyOne.test.js
@@ -1,17 +1,17 @@
-import orderByProps from "..proxy.js";
+import orderByProps from "../proxyOne";
 
-test('проверка обычной работы функции', () => {
+test('seatch', () => {
     const obj = {
         name: 'мечник', health: 10, level: 2, attack: 80, defence: 40,
     };
     const args = ['name', 'level'];
-    const received = sortedArray(obj, args);
+    const received = orderByProps(obj, args);
     const expected = [
-        { key: 'attack', value: 80 },
-        { key: 'level', value: 2 },
-        { key: 'defence', value: 40 },
-        { key: 'health', value: 10 },
         { key: 'name', value: 'мечник' },
+        { key: 'level', value: 2 },
+        { key: 'attack', value: 80 },
+        { key: 'defence', value: 40 },
+        { key: 'health', value: 10 }
     ];
     expect(received).toEqual(expected);
 });
