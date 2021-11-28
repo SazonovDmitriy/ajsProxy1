9da92da8737b77e90c4ac76bb3a56eed2bfc68bc
SazonovDmitriy <sazonovd21@mail.ru>
2021-11-28 15:30:02 +0300

    Help

--- src/js/proxyOne.js
+++ src/js/proxyOne.js
@@ -1,20 +1,19 @@
 export default function orderByProps(obj, sortDirection) {
     let propArray = [];
-    for (const [key, value] of Object.entries(obj)) {
-        const item = { key, value };
-        propArray.push(item);
+    for (const key in obj) {
+        propArray.push();
+    };
+
+    const arrResult = [];
+
+    function sortArr (array) {
+        return array.sort((a,b) => b. - a.health);
+    };
+
+    for (let i = 0; i < propArray.length; i += 1) {
+        const k = propArray[i];
     }
-    propArray.sort((a,b) => {
-        if (a.key > b.key) {
-            return 1;
-        } else {
-            return -1;
-        };
-    });
-    for (let i = 0; i < sortDirection.length; i++) {
-        let element = propArray.find(item => item.key === sortDirection[i]);
-        const index = propArray.indexOf(element);
-        propArray.splice(i, 0, propArray.splice(index, 1)[0]);
-    }
-    return propArray;
+    
+
+    return arrResult;
 }
