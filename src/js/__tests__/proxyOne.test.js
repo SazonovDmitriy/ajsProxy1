a935231dbb3de05835203d1cca2aeb151fed3f9d
SazonovDmitriy <sazonovd21@mail.ru>
2021-12-09 12:22:22 +0300

    1

--- src/js/__tests__/proxyOne.test.js
+++ src/js/__tests__/proxyOne.test.js
@@ -1,6 +1,6 @@
 import orderByProps from "../proxyOne";
 
-test('seatch', () => {
+test('search', () => {
     const obj = {
         name: 'мечник', health: 10, level: 2, attack: 80, defence: 40,
     };
@@ -11,27 +11,23 @@ test('seatch', () => {
         { key: 'level', value: 2 },
         { key: 'attack', value: 80 },
         { key: 'defence', value: 40 },
-        { key: 'health', value: 10 }
+        { key: 'health', value: 10 },
     ];
     expect(received).toEqual(expected);
 });
 
-// test('seatch', () => {
-//     const obj = {
-//         name: 'мечник', health: 10, level: 2, attack: 80, defence: 40,
-//     };
-
-//     const received = obj.hasOwnProperty('key')
-
-//     expect(received).toEqual(true);
-// });
-
-// test('seatch', () => {
-//     const obj = {
-//         name: 'мечник', health: 10, level: 2, attack: 80, defence: 40,
-//     };
-//     const args = null
-//     const received = orderByProps(obj, args);
-
-//     expect(received).toBe(false);
-// });
+test('search key', () => {
+    const obj = {
+        name: 'мечник', health: 10, level: 2, attack: 80, defence: 40,
+    };
+    const args = [undefined];
+    const received = orderByProps(obj, args);
+    const expected = [
+        { key: 'attack', value: 80 },
+        { key: 'defence', value: 40 },
+        { key: 'health', value: 10 },
+        { key: 'level', value: 2 },
+        { key: 'name', value: 'мечник' },
+    ];
+    expect(received).toEqual(expected);
+});
